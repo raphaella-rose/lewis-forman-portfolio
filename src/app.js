9f5c634931a5d271edd319399867a003a18ff7c2
raphaella-rose <raphaella.rose1@gmail.com>
2022-04-17 20:31:04 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,7 @@ import data from "./data.json" assert {type: 'json'};
 
 
 function displayPortfolio(data) {
-  let portfolioElement = document.querySelector("#portfolio")
-  let portfolioHTML = "";
-  for (let show of data)  {
-  portfolioHTML =  portfolioHTML +
-    ` Test
-    `;
-    portfolioElement.innerHTML = portfolioHTML;
-  }
-
+alert("Test")
     }
 
 
